3df69eb035b8074889ab85629d5c46738b5f0c73
Takhyun Kim <kimkih1218@gmail.com>
2022-06-26 00:45:57 +0900

    feat: add BOTTOM_TAB_NAVIGATION_NAME Constant

--- src/constants/route.ts
+++ src/constants/route.ts
@@ -11,3 +11,10 @@ export const ROUTE_PATH = {
 export const NAVIGATION_NAME = {
   BOTTOM_TAB_NAVIGATION: 'bottomTabNavigation',
 };
+
+export const BOTTOM_TAB_NAVIGATION_NAME = {
+  HOME: '홈',
+  SUCCESS: '성공',
+  SOCIAL: '소셜',
+  MY_PAGE: '내정보',
+};

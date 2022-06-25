278450933048e9ad241dfeb2b773f2e04591e0cd
Takhyun Kim <kimkih1218@gmail.com>
2022-06-26 00:46:49 +0900

    feat: apply BottomTabNavigation to UserScreenNavigation

--- src/navigations/UserScreenNavigation/index.tsx
+++ src/navigations/UserScreenNavigation/index.tsx
@@ -2,31 +2,24 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import {
-  Social,
-  MyPage,
-  HabitPlus,
-  GrowingTree,
-  HabitComplete,
-  HabitListHome,
-  UserStatistics,
-} from '../../screens';
+import { HabitPlus, HabitComplete } from '../../screens';
 
-import { ROUTE_PATH } from '../../constants/route';
+import { BottomTabNavigation } from '../';
+
+import { ROUTE_PATH, NAVIGATION_NAME } from '../../constants/route';
 
 const { Screen, Navigator } = createStackNavigator();
 
 const UserScreenNavigation = () => {
   return (
     <NavigationContainer>
-      <Navigator initialRouteName={ROUTE_PATH.HABIT_LIST_HOME}>
-        <Screen name={ROUTE_PATH.SOCIAL} component={Social} />
-        <Screen name={ROUTE_PATH.MY_PAGE} component={MyPage} />
+      <Navigator screenOptions={{ headerShown: false }}>
+        <Screen
+          name={NAVIGATION_NAME.BOTTOM_TAB_NAVIGATION}
+          component={BottomTabNavigation}
+        />
         <Screen name={ROUTE_PATH.HABIT_PLUS} component={HabitPlus} />
-        <Screen name={ROUTE_PATH.GROWING_TREE} component={GrowingTree} />
         <Screen name={ROUTE_PATH.HABIT_COMPLETE} component={HabitComplete} />
-        <Screen name={ROUTE_PATH.HABIT_LIST_HOME} component={HabitListHome} />
-        <Screen name={ROUTE_PATH.USER_STATISTICS} component={UserStatistics} />
       </Navigator>
     </NavigationContainer>
   );

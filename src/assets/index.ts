cca9d770d3df4bf8fe369155269a38f7e59074b5
Takhyun Kim <kimkih1218@gmail.com>
2022-06-26 14:37:16 +0900

    feat: add bottom tab navigation svg icon

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+import HomeIcon from './home.svg';
+import TreeIcon from './tree.svg';
+import UserIcon from './user.svg';
+import SocialIcon from './social.svg';
+import AnalyticsIcon from './analytics.svg';
+
+export { HomeIcon, TreeIcon, UserIcon, SocialIcon, AnalyticsIcon };

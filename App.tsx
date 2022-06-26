8909e15bcddfd35e649ccc17c105b07e79255a91
Takhyun Kim <kimkih1218@gmail.com>
2022-06-26 20:46:15 +0900

    feat: apply RecoilRoot to App

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { RecoilRoot } from 'recoil';
 
 import { UserScreenNavigation } from './src/navigations';
 
 const App = () => {
-  return <UserScreenNavigation />;
+  return (
+    <RecoilRoot>
+      <UserScreenNavigation />
+    </RecoilRoot>
+  );
 };
 
 export default App;

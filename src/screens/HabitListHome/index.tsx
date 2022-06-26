f7efbb1b36953cdb0572bfeb169d133f37a81e35
Takhyun Kim <kimkih1218@gmail.com>
2022-06-26 19:41:22 +0900

    feat: apply WeekViewer container to HabitListHome

--- src/screens/HabitListHome/index.tsx
+++ src/screens/HabitListHome/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import { WeekViewer } from '../../containers';
+
 import { ScreenContainer } from '../';
 
 const HabitListHome = () => {
   return (
     <ScreenContainer>
+      <WeekViewer />
       <Text>HabitListHome</Text>
     </ScreenContainer>
   );

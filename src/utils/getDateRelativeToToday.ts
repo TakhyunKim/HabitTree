299484fd12c505517f618267d7be0b695990c0e4
Takhyun Kim <kimkih1218@gmail.com>
2022-06-26 20:34:16 +0900

    refactor: add today constant to getDateRelativeToToday

--- src/utils/getDateRelativeToToday.ts
+++ src/utils/getDateRelativeToToday.ts
@@ -2,9 +2,9 @@
  * 오늘 날짜를 기준으로 원하는 Date 객체를 return 해주는 function
  */
 const getDateRelativeToToday = (numberOfDaysToMove: number) => {
-  return new Date(
-    new Date().setDate(new Date().getDate() + numberOfDaysToMove),
-  );
+  const today = new Date();
+
+  return new Date(today.setDate(today.getDate() + numberOfDaysToMove));
 };
 
 export default getDateRelativeToToday;

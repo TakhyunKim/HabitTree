7cc0815a0936e62a1fb12a622121e98ae0753501
Takhyun Kim <kimkih1218@gmail.com>
2022-06-28 22:34:03 +0900

    feat: apply animation to HabitPlusButtons container

--- src/containers/HabitPlusButtons/HabitPlusButtons.styles.ts
+++ src/containers/HabitPlusButtons/HabitPlusButtons.styles.ts
@@ -1,12 +1,17 @@
 import { StyleSheet } from 'react-native';
 
-export const styles = StyleSheet.create({
-  habitPlusButtonsContainer: {
-    position: 'absolute',
-    bottom: 20,
-    right: 15,
-  },
-  habitPlusButton: {
-    alignItems: 'flex-end',
-  },
-});
+export const styles = (isActiveOfPlus: boolean) =>
+  StyleSheet.create({
+    habitPlusButtonsContainer: {
+      alignItems: 'flex-end',
+      position: 'absolute',
+      bottom: 20,
+      right: 15,
+    },
+    habitPlusButton: {
+      width: 50,
+      height: 50,
+      alignItems: 'flex-end',
+      transform: [{ rotate: isActiveOfPlus ? '45deg' : '0deg' }],
+    },
+  });
--- src/containers/HabitPlusButtons/index.tsx
+++ src/containers/HabitPlusButtons/index.tsx
@@ -1,27 +1,101 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Animated } from 'react-native';
 
 import { HabitPlus, HabitPlusWithText } from '../../components';
+
+import { useFlagAnimation } from '../../hooks';
+import { getAnimationMultiStyle } from '../../utils';
+import { AnimationStyleInfo } from '../../types';
 
 import { styles } from './HabitPlusButtons.styles';
 
 const HabitPlusButtons = () => {
+  const [isActiveOfPlus, setIsActiveOfPlus] = useState<boolean>(false);
+
+  const style = styles(isActiveOfPlus);
+
+  const weekHabitAnimatedValue = useFlagAnimation({
+    flag: isActiveOfPlus,
+    duration: 1100,
+    animationToValue: 20,
+  });
+  const todayHabitAnimatedValue = useFlagAnimation({
+    flag: isActiveOfPlus,
+    duration: 1000,
+    animationToValue: 15,
+  });
+
+  const animationStylesInfo: AnimationStyleInfo[] = [
+    {
+      styleName: 'translateY',
+      animatedValue: weekHabitAnimatedValue,
+      inputRange: [0, 1],
+      outputRange: [1, 10],
+    },
+    {
+      styleName: 'opacity',
+      animatedValue: weekHabitAnimatedValue,
+      inputRange: [0, 10],
+      outputRange: [10, 0],
+    },
+    {
+      styleName: 'translateY',
+      animatedValue: todayHabitAnimatedValue,
+      inputRange: [0, 1],
+      outputRange: [1, 6.5],
+    },
+    {
+      styleName: 'opacity',
+      animatedValue: todayHabitAnimatedValue,
+      inputRange: [0, 10],
+      outputRange: [10, 0],
+    },
+  ];
+
+  const [
+    weekHabitTranslateY,
+    weekHabitOpacity,
+    todayHabitTranslateY,
+    todayHabitOpacity,
+  ] = getAnimationMultiStyle({ animationStylesInfo });
+
+  const handleHabitPlusButtonClick = () => {
+    setIsActiveOfPlus(prevState => !prevState);
+  };
+
   return (
-    <View style={styles.habitPlusButtonsContainer}>
-      <HabitPlusWithText
-        description="주간 목표 추가"
-        backgroundColor="#AFA800"
-        onPress={() => console.log('주간 목표 추가')}
-      />
-      <HabitPlusWithText
-        description="오늘 목표 추가"
-        backgroundColor="#EDE636"
-        onPress={() => console.log('오늘 목표 추가')}
-      />
-      <View style={styles.habitPlusButton}>
+    <View style={style.habitPlusButtonsContainer}>
+      <Animated.View
+        style={[
+          {
+            transform: [{ translateY: weekHabitTranslateY }],
+            opacity: weekHabitOpacity,
+          },
+        ]}>
+        <HabitPlusWithText
+          description="주간 목표 추가"
+          backgroundColor="#AFA800"
+          onPress={() => console.log('주간 목표 추가')}
+        />
+      </Animated.View>
+      <Animated.View
+        style={[
+          {
+            transform: [{ translateY: todayHabitTranslateY }],
+            opacity: todayHabitOpacity,
+          },
+        ]}>
+        <HabitPlusWithText
+          description="오늘 목표 추가"
+          backgroundColor="#EDE636"
+          onPress={() => console.log('오늘 목표 추가')}
+        />
+      </Animated.View>
+
+      <View style={style.habitPlusButton}>
         <HabitPlus
           backgroundColor="#FCF664"
-          onPress={() => console.log('plus')}
+          onPress={handleHabitPlusButtonClick}
         />
       </View>
     </View>

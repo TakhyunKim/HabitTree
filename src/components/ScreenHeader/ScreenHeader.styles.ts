a26672aac13eafdaee9280cd28080ee50112310c
Takhyun Kim <kimkih1218@gmail.com>
2022-06-29 09:44:14 +0900

    feat: add ScreenHeader component

---
+++ src/components/ScreenHeader/ScreenHeader.styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  screenHeaderContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '15%',
+    marginBottom: 10,
+    backgroundColor: '#ffffff',
+  },
+  screenHeaderTitleText: {
+    marginTop: 30,
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: '#414141',
+  },
+});

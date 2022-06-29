5a31ba5b64c9ea32211b6bc39fac6f8d2dcaf867
Takhyun Kim <kimkih1218@gmail.com>
2022-06-29 15:38:23 +0900

    feat: apply LogBox ignore to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { LogBox } from 'react-native';
 import { RecoilRoot } from 'recoil';
 
 import { UserScreenNavigation } from './src/navigations';
+
+// Date 객체를 발생하는 오류를 무시하기 위해 추가
+// https://reactnavigation.org/docs/troubleshooting/#i-get-the-warning-non-serializable-values-were-found-in-the-navigation-state 참고
+LogBox.ignoreLogs([
+  'Non-serializable values were found in the navigation state',
+]);
 
 const App = () => {
   return (

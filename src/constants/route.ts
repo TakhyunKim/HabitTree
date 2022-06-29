d88866daf72f6193699235301711ac8eaed2bfdb
Takhyun Kim <kimkih1218@gmail.com>
2022-06-29 12:48:04 +0900

    feat: apply as const to route constant

--- src/constants/route.ts
+++ src/constants/route.ts
@@ -6,15 +6,15 @@ export const ROUTE_PATH = {
   MY_PAGE: 'myPage',
   SOCIAL: 'social',
   USER_STATISTICS: 'userStatistics',
-};
+} as const;
 
 export const NAVIGATION_NAME = {
   BOTTOM_TAB_NAVIGATION: 'bottomTabNavigation',
-};
+} as const;
 
 export const BOTTOM_TAB_NAVIGATION_NAME = {
   HOME: '홈',
   SUCCESS: '성공',
   SOCIAL: '소셜',
   MY_PAGE: '내정보',
-};
+} as const;

abe58e7e4ef70106f119c469824bd5bae752cc4e
Takhyun Kim <kimkih1218@gmail.com>
2022-06-29 15:39:57 +0900

    feat: add HabitComplete container

---
+++ src/containers/HabitComplete/index.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+
+import { TextInput, HabitAddOption, HabitSettingBox } from '../../components';
+
+import { getKoreaDate } from '../../utils';
+
+import { HabitCompleteProps } from './HabitComplete.types';
+import { styles } from './HabitComplete.styles';
+
+const HabitComplete = ({
+  date,
+  habitTitle,
+  isPrivate,
+  mainImage,
+  description,
+}: HabitCompleteProps) => {
+  const [text, setText] = useState<string>(habitTitle);
+  const [certiImage] = useState<string | null>(null);
+
+  return (
+    <View style={styles.habitAddContainer}>
+      <TextInput placeHolader="목표 타이틀" value={text} onChange={setText} />
+      <HabitSettingBox settingTitle="기본 설정">
+        <HabitAddOption optionTitle="날짜" hasborderBottom>
+          <Text style={styles.habitAddOptionText}>{getKoreaDate(date)}</Text>
+        </HabitAddOption>
+        <HabitAddOption optionTitle="상세 내용" hasborderBottom>
+          <Text
+            style={
+              styles[
+                description
+                  ? 'habitAddOptionText'
+                  : 'habitAddOptionTextWithOpacity'
+              ]
+            }>
+            추가하기
+          </Text>
+        </HabitAddOption>
+        <HabitAddOption optionTitle="공개">
+          <Text style={styles.habitAddOptionText}>
+            {isPrivate ? '비공개' : '공개'}
+          </Text>
+        </HabitAddOption>
+      </HabitSettingBox>
+      <HabitSettingBox settingTitle="추가 설정">
+        <HabitAddOption optionTitle="대표 이미지" hasborderBottom>
+          <Text
+            style={
+              styles[
+                mainImage
+                  ? 'habitAddOptionText'
+                  : 'habitAddOptionTextWithOpacity'
+              ]
+            }>
+            첨부하기
+          </Text>
+        </HabitAddOption>
+        <HabitAddOption optionTitle="인증 이미지" hasborderBottom>
+          <Text
+            style={
+              styles[
+                certiImage
+                  ? 'habitAddOptionText'
+                  : 'habitAddOptionTextWithOpacity'
+              ]
+            }>
+            첨부하기
+          </Text>
+        </HabitAddOption>
+      </HabitSettingBox>
+    </View>
+  );
+};
+
+export default HabitComplete;
--- src/containers/index.ts
+++ src/containers/index.ts
@@ -2,14 +2,18 @@ import HabitAdd from './HabitAdd';
 import HabitList from './HabitList';
 import WeekViewer from './WeekViewer';
 import ScreenHeader from './ScreenHeader';
+import HabitComplete from './HabitComplete';
 import DayHabitSubmit from './DayHabitSubmit';
 import HabitPlusButtons from './HabitPlusButtons';
+import HabitCompleteSubmit from './HabitCompleteSubmit';
 
 export {
   HabitAdd,
   HabitList,
   WeekViewer,
   ScreenHeader,
+  HabitComplete,
   DayHabitSubmit,
   HabitPlusButtons,
+  HabitCompleteSubmit,
 };

402bd5bee6fd012a3c5fc0e83694e90f11e862ab
Takhyun Kim <kimkih1218@gmail.com>
2022-06-29 13:53:34 +0900

    feat: add text style to HabitAdd container

--- src/containers/HabitAdd/index.tsx
+++ src/containers/HabitAdd/index.tsx
@@ -17,37 +17,29 @@ const HabitAdd = ({ habitAddOption }: HabitAddProps) => {
   const today = useRecoilValue(habitDay);
 
   return (
-    <View style={styles.HabitAddContainer}>
+    <View style={styles.habitAddContainer}>
       <TextInput placeHolader="목표 타이틀" value={text} onChange={setText} />
       <HabitSettingBox settingTitle="기본 설정">
         <HabitAddOption
           optionTitle={habitAddOption === 'day' ? '날짜' : '목표 시간 날짜'}
           hasborderBottom>
-          <Text style={{ fontWeight: 'bold', fontSize: 12 }}>
-            {getKoreaDate(today)}
-          </Text>
+          <Text style={styles.habitAddOptionText}>{getKoreaDate(today)}</Text>
         </HabitAddOption>
         <HabitAddOption optionTitle="상세 내용" hasborderBottom>
-          <Text style={{ fontWeight: 'bold', fontSize: 12, opacity: 0.3 }}>
-            추가하기
-          </Text>
+          <Text style={styles.habitAddOptionTextWithOpacity}>추가하기</Text>
         </HabitAddOption>
         {habitAddOption === 'week' && (
           <HabitAddOption optionTitle="반복" hasborderBottom>
-            <Text style={{ fontWeight: 'bold', fontSize: 12, opacity: 0.3 }}>
-              매일
-            </Text>
+            <Text style={styles.habitAddOptionTextWithOpacity}>매일</Text>
           </HabitAddOption>
         )}
         <HabitAddOption optionTitle="공개">
-          <Text style={{ fontWeight: 'bold', fontSize: 12 }}>공개하기</Text>
+          <Text style={styles.habitAddOptionText}>공개하기</Text>
         </HabitAddOption>
       </HabitSettingBox>
       <HabitSettingBox settingTitle="추가 설정">
         <HabitAddOption optionTitle="대표 이미지" hasborderBottom>
-          <Text style={{ fontWeight: 'bold', fontSize: 12, opacity: 0.3 }}>
-            첨부하기
-          </Text>
+          <Text style={styles.habitAddOptionTextWithOpacity}>첨부하기</Text>
         </HabitAddOption>
       </HabitSettingBox>
     </View>

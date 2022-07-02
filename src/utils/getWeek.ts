a0f0d26e6272732e62f09006daa95882b711750f
Takhyun Kim <kimkih1218@gmail.com>
2022-07-02 09:58:10 +0900

    fix: modify getWeek saturdaym sunday setDate value

--- src/utils/getWeek.ts
+++ src/utils/getWeek.ts
@@ -2,14 +2,14 @@ import { DateInfo } from '../types';
 const getWeek = (): DateInfo => {
   const today = new Date();
   const first = today.getDate() - today.getDay() + 1;
-
+  console.log(first);
   const monday = new Date(new Date().setDate(first));
   const tuesday = new Date(new Date().setDate(first + 1));
   const wednesday = new Date(new Date().setDate(first + 2));
   const thursday = new Date(new Date().setDate(first + 3));
   const friday = new Date(new Date().setDate(first + 4));
-  const saturday = new Date(new Date().setDate(first + 6));
-  const sunday = new Date(new Date().setDate(first + 7));
+  const saturday = new Date(new Date().setDate(first + 5));
+  const sunday = new Date(new Date().setDate(first + 6));
 
   const dateInfo: DateInfo = {
     월: monday,

38c3437fcafd7d0dfa98d1bf4d47aac17be26495
Takhyun Kim <kimkih1218@gmail.com>
2022-07-02 23:31:27 +0900

    feat: apply ScreenHeader to UserStatistics screen

--- src/screens/UserStatistics/index.tsx
+++ src/screens/UserStatistics/index.tsx
@@ -3,9 +3,12 @@ import { Text } from 'react-native';
 
 import { ScreenContainer } from '../';
 
+import { ScreenHeader } from '../../containers';
+
 const UserStatistics = () => {
   return (
     <ScreenContainer>
+      <ScreenHeader headerTitle="나의 성공 지표" />
       <Text>UserStatistics</Text>
     </ScreenContainer>
   );

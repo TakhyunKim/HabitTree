50f74d340683fcc530c10e2ce9d483fbe766f3a5
Takhyun Kim <kimkih1218@gmail.com>
2022-07-04 00:37:29 +0900

    feat: apply PortraitModeProgressBarList to Statistics container

--- src/containers/Statistics/index.tsx
+++ src/containers/Statistics/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { View } from 'react-native';
 
-import { WhiteBoxWithSubTitle, ProgressBar } from '../../components';
+import {
+  WhiteBoxWithSubTitle,
+  ProgressBar,
+  PortraitModeProgressBarList,
+} from '../../components';
 
 import { styles } from './Statistics.styles';
 
@@ -10,6 +14,19 @@ const Statistics = () => {
     <View style={styles.statisticsContainer}>
       <WhiteBoxWithSubTitle subTitle="오늘 지표">
         <ProgressBar percent={30} />
+      </WhiteBoxWithSubTitle>
+      <WhiteBoxWithSubTitle subTitle="주간 지표">
+        <PortraitModeProgressBarList
+          weekPercent={{
+            월: 70,
+            화: 30,
+            수: 90,
+            목: 100,
+            금: 10,
+            토: 0,
+            일: 0,
+          }}
+        />
       </WhiteBoxWithSubTitle>
     </View>
   );

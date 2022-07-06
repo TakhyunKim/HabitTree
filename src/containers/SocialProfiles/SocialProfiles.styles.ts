8c9a28eaadbd1ff0bcdd44fa11bcc0c5e929ec46
Takhyun Kim <kimkih1218@gmail.com>
2022-07-06 20:07:42 +0900

    feat: add SocialProfiles container

---
+++ src/containers/SocialProfiles/SocialProfiles.styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  socialContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    width: '90%',
+    borderBottomColor: '#D9D9D9',
+    borderBottomWidth: 4,
+  },
+});

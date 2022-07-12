78e297068068aebab5f18ab553a5311f67f59ca4
Takhyun Kim <kimkih1218@gmail.com>
2022-07-13 00:11:04 +0900

    feat: add cloud, clouds, water-can-fill svg file

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,10 +3,13 @@ import LandIcon from './land.svg';
 import TreeIcon from './tree.svg';
 import UserIcon from './user.svg';
 import PlusIcon from './plus.svg';
+import CloudIcon from './cloud.svg';
+import CloudsIcon from './clouds.svg';
 import SocialIcon from './social.svg';
 import TeaLeatIcon from './tea-leaf.svg';
 import AnalyticsIcon from './analytics.svg';
 import WateringCan from './watering-can.svg';
+import WateringFillCan from './watering-can-fill.svg';
 
 export {
   HomeIcon,
@@ -14,8 +17,11 @@ export {
   TreeIcon,
   UserIcon,
   PlusIcon,
+  CloudIcon,
+  CloudsIcon,
   SocialIcon,
   TeaLeatIcon,
   WateringCan,
   AnalyticsIcon,
+  WateringFillCan,
 };

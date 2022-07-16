ce9a798fdca173fcbce4611edd05c3aeaa9ba7ce
Takhyun Kim <kimkih1218@gmail.com>
2022-07-16 16:01:14 +0900

    feat: add handleHabitItemClick function and apply to HabitItem component

--- src/containers/HabitList/index.tsx
+++ src/containers/HabitList/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { View } from 'react-native';
+import { useNavigation, NavigationProp } from '@react-navigation/native';
 import { useRecoilValue } from 'recoil';
 
 import { HabitItem } from '../../components';
+
+import { HabitItem as HabitItemType, HabitCompleteParams } from '../../types';
 
 import { filteredHabitListState } from '../../recoil/selectors';
 
@@ -11,11 +14,24 @@ import { styles } from './HabitList.styles';
 const HabitList = () => {
   const filteredHabitList = useRecoilValue(filteredHabitListState);
 
+  const navigation = useNavigation<NavigationProp<HabitCompleteParams>>();
+
+  const handleHabitItemClick = (habitItemInfo: HabitItemType) => {
+    navigation.navigate('habitComplete', {
+      habitTitle: habitItemInfo.title,
+      ...habitItemInfo,
+    });
+  };
+
   return (
     <View style={styles.habitListContainer}>
-      {filteredHabitList.map(({ title, isComplete }) => (
+      {filteredHabitList.map(habitItemInfo => (
         // TODO: mock data 에는 unique Key 값이 존재하지 않아 title 로 지정, 추후 변경 필요
-        <HabitItem key={title} title={title} isComplete={isComplete} />
+        <HabitItem
+          {...habitItemInfo}
+          key={habitItemInfo.title}
+          onPress={() => handleHabitItemClick(habitItemInfo)}
+        />
       ))}
     </View>
   );

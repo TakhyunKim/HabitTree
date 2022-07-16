8f6a266635c87f91e814eabaf1688a6740bf8072
Takhyun Kim <kimkih1218@gmail.com>
2022-07-16 19:20:42 +0900

    feat: add background style to HabitPlusButtons container

--- src/containers/HabitPlusButtons/HabitPlusButtons.styles.ts
+++ src/containers/HabitPlusButtons/HabitPlusButtons.styles.ts
@@ -2,6 +2,13 @@ import { StyleSheet } from 'react-native';
 
 export const styles = (isActiveOfPlus: boolean) =>
   StyleSheet.create({
+    habitPlusButtonsBackground: {
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      backgroundColor: isActiveOfPlus ? 'rgba(217, 217, 217, 0.6)' : 'none',
+      zIndex: isActiveOfPlus ? 0 : -1,
+    },
     habitPlusButtonsContainer: {
       alignItems: 'flex-end',
       position: 'absolute',

c55f8175cc6dc5a4114a81a37cfa19aeb17b6462
Takhyun Kim <kimkih1218@gmail.com>
2022-07-21 23:05:23 +0900

    feat: add animation to GrowingTreeImage button

--- src/containers/GrowingTreeImage/index.tsx
+++ src/containers/GrowingTreeImage/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { View, Animated, PanResponder, TouchableOpacity } from 'react-native';
 
 import {
   LandIcon,
@@ -12,15 +12,43 @@ import {
 import { styles } from './GrowingTreeImage.styles';
 
 const GrowingTreeImage = () => {
+  const [isWateringCanActive, setIsWateringCanActive] =
+    useState<boolean>(false);
+
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: Animated.event([null, { dx: pan.x, dy: pan.y }], {
+        useNativeDriver: false,
+      }),
+      onPanResponderRelease: () => {
+        Animated.spring(pan, {
+          toValue: { x: 0, y: 0 },
+          useNativeDriver: false,
+        }).start();
+      },
+    }),
+  ).current;
+
+  const handleWWateringCanClick = () => {
+    setIsWateringCanActive(prevState => !prevState);
+  };
+
   return (
     <View style={styles.growingTreeImageContainer}>
       <TeaLeatIcon style={styles.teaLeatIcon} width={40} height={40} />
       <LandIcon style={styles.landIcon} width="120%" height="100%" />
       <CloudIcon style={styles.cloudIcon} width="30%" height="30%" />
       <CloudsIcon style={styles.cloudsIcon} width="30%" height="30%" />
-      <View style={styles.waterIconWrapper}>
-        <WateringCan width={30} height={30} fill="#000000" />
-      </View>
+      <Animated.View {...panResponder.panHandlers} style={[pan.getLayout()]}>
+        <TouchableOpacity
+          style={styles.waterIconWrapper}
+          onPress={handleWWateringCanClick}>
+          <WateringCan width={30} height={30} fill="#000000" />
+        </TouchableOpacity>
+      </Animated.View>
     </View>
   );
 };

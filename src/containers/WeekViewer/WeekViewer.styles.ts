b10b21be3c3a5363d07a19deed9c8a5dd1d1d697
Takhyun Kim <kimkih1218@gmail.com>
2022-07-25 00:11:12 +0900

    feat: apply ScrollView to WeekViewer container

--- src/containers/WeekViewer/WeekViewer.styles.ts
+++ src/containers/WeekViewer/WeekViewer.styles.ts
@@ -1,22 +1,26 @@
 import { StyleSheet } from 'react-native';
 
-export const styles = StyleSheet.create({
-  WeekViewerContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
-    marginBottom: 10,
-    paddingTop: 50,
-    backgroundColor: '#ffffff',
-  },
-  WeekViewerChildrenContainer: {
-    width: '90%',
-  },
-  DayViewerWrapper: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
-  },
-});
+export const styles = (width: number) =>
+  StyleSheet.create({
+    weekViewerContainer: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '100%',
+      marginBottom: 10,
+      paddingTop: 50,
+      backgroundColor: '#ffffff',
+    },
+    yearAndMonthWrapper: {
+      width: '90%',
+    },
+    weekViewerScrollWrapper: {
+      width: '200%',
+    },
+    dayViewerWrapper: {
+      width,
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  });

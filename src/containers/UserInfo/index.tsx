d2a113f872d6467bf52581f4a7825c7b08a880db
Takhyun Kim <kimkih1218@gmail.com>
2022-07-30 00:06:47 +0900

    feat: add navigation logic to UserInfo profile edit button click

--- src/containers/UserInfo/index.tsx
+++ src/containers/UserInfo/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { Profile, Button } from '../../components';
 
 import { styles } from './UserInfo.styles';
 
 const UserInfo = () => {
+  const navigation = useNavigation();
+
+  const handleHabitItemClick = () => {
+    navigation.navigate('profileEdit');
+  };
+
   return (
     <View style={styles.userInfoContainer}>
       <View style={styles.userInfoProfileContainer}>
         <Profile name="김탁현" plantGrade="나무" plantHeight={7} image="none" />
       </View>
-      <Button title="프로필 편집" onPress={() => console.log('프로필 편집')} />
+      <Button title="프로필 편집" onPress={handleHabitItemClick} />
       <Button title="나의 사진첩" onPress={() => console.log('나의 사진첩')} />
       <Button
         textColor="#C62B2B"

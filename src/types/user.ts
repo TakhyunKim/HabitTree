2a516303f682d29a181e55ec9a98bc4e90909174
Takhyun Kim <kimkih1218@gmail.com>
2022-07-29 22:15:43 +0900

    feat: add Unit type and User interface

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,1 +1,10 @@
 export type PlantGrade = '새싹' | '나무';
+
+export type Unit = 'mm' | 'M';
+
+export interface User {
+  name: string;
+  grade: PlantGrade;
+  growingStatus: number;
+  unit: Unit;
+}

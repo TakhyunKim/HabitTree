683340e0c1a819a602591eb39260e698e63359f0
Takhyun Kim <kimkih1218@gmail.com>
2022-07-29 22:07:12 +0900

    feat: modify ProfileHeader container style

--- src/containers/ProfileHeader/ProfileHeader.styles.ts
+++ src/containers/ProfileHeader/ProfileHeader.styles.ts
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   profileHeaderContainer: {
-    width: '100%',
-    height: 100,
+    width: '90%',
+    height: 50,
   },
 });

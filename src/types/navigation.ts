619395d1988ce82a6ad5c994660ed4e0a87cb090
Takhyun Kim <kimkih1218@gmail.com>
2022-08-01 22:46:11 +0900

    feat: add RootStateParamList, BottomTabNavigationParamList type

--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -54,3 +54,30 @@ export type HabitAddScreenNavigationProp = CompositeScreenProps<
   BottomTabScreenProps<BottomTabParams, 'habitListHome'>,
   StackScreenProps<StackParams>
 >;
+
+// TODO: Navigation type 정리 필요
+
+// Root Navigation type 에 사용하는 Root Stack type
+export type RootStackParamList = {
+  [NAVIGATION_NAME.BOTTOM_TAB_NAVIGATION]: undefined;
+  [ROUTE_PATH.PROFILE_EDIT]: undefined;
+  [ROUTE_PATH.HABIT_PLUS]: {
+    habitPlusOption: HabitPlusOption;
+  };
+  [ROUTE_PATH.HABIT_COMPLETE]: {
+    date: Date;
+    title: string;
+    isPrivate: boolean;
+    mainImage?: string;
+    description?: string;
+  };
+};
+
+// Bottom Tab navigation 에 사용하는 type
+export type BottomTabNavigationParamList = {
+  [ROUTE_PATH.HABIT_LIST_HOME]: undefined;
+  [ROUTE_PATH.USER_STATISTICS]: undefined;
+  [ROUTE_PATH.GROWING_TREE]: undefined;
+  [ROUTE_PATH.SOCIAL]: undefined;
+  [ROUTE_PATH.MY_PAGE]: undefined;
+};

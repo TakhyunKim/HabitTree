ee2f866a639c2dcb75e61b6b2e6a4c905d67e530
Takhyun Kim <kimkih1218@gmail.com>
2022-08-01 22:48:08 +0900

    feat: delete unused type to navigation

--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -1,59 +1,6 @@
-import { CompositeScreenProps } from '@react-navigation/native';
-import { StackScreenProps } from '@react-navigation/stack';
-import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
-
 import { ROUTE_PATH, NAVIGATION_NAME } from '@constants/route';
 
 import { HabitPlusOption } from '@types';
-
-export type RoutePathKeysType = keyof typeof ROUTE_PATH;
-
-export type RoutePathValuesType = typeof ROUTE_PATH[RoutePathKeysType];
-
-export type NavigationNameKeysType = keyof typeof NAVIGATION_NAME;
-
-export type NavigationNameValuesType =
-  typeof NAVIGATION_NAME[NavigationNameKeysType];
-
-export type BottomTabRoutePathValues = Exclude<
-  RoutePathValuesType,
-  'habitPlus'
->;
-
-export type StackRoutePathValues = Extract<
-  RoutePathValuesType,
-  'habitPlus' | 'habitComplete' | 'profileEdit'
->;
-
-export type HabitPlusValue = Extract<RoutePathValuesType, 'habitPlus'>;
-
-export type HabitCompleteValue = Extract<RoutePathValuesType, 'habitComplete'>;
-
-export type HabitCompleteParams = {
-  [habitComplete in HabitCompleteValue]: {
-    date: Date;
-    habitTitle: string;
-    isPrivate: boolean;
-    mainImage?: string;
-    description?: string;
-  };
-};
-
-export type HabitPlusParams = {
-  [habitPlus in HabitPlusValue]: { habitPlusOption: HabitPlusOption };
-};
-export type NavigationParams = Record<NavigationNameValuesType, undefined>;
-export type StackParams = Record<StackRoutePathValues, undefined>;
-export type BottomTabParams = Record<BottomTabRoutePathValues, undefined>;
-
-export type BottomTabNavigationParams = BottomTabScreenProps<BottomTabParams>;
-
-export type StackNavigationParams = StackScreenProps<StackParams>;
-
-export type HabitAddScreenNavigationProp = CompositeScreenProps<
-  BottomTabScreenProps<BottomTabParams, 'habitListHome'>,
-  StackScreenProps<StackParams>
->;
 
 // TODO: Navigation type 정리 필요
 

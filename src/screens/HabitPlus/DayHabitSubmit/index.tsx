147fe4c8a70760cdd04eb4d00dde79e6b7b4de7e
Takhyun Kim <kimkih1218@gmail.com>
2022-08-06 23:31:39 +0900

    feat: pop navigation stack to submit button onPress prop

--- src/screens/HabitPlus/DayHabitSubmit/index.tsx
+++ src/screens/HabitPlus/DayHabitSubmit/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { useNavigation, StackActions } from '@react-navigation/native';
 
 import { HabitPlusSubmitButton } from '@components';
 
+import { NavigationProp } from '@types';
+
 const DayHabitSubmit = () => {
+  const navigation = useNavigation<NavigationProp>();
+
+  const handleSubmitButtonClick = () => {
+    const popAction = StackActions.pop();
+
+    navigation.dispatch(popAction);
+  };
+
   return (
     <HabitPlusSubmitButton
       buttonTitle="목표 시작하기"
-      onPress={() => console.log('목표 제출')}
+      onPress={handleSubmitButtonClick}
     />
   );
 };
